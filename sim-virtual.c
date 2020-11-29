0460764cdbeef97664cc257b3cb11122e792b9b0
camilaceles <52015570+camilaceles@users.noreply.github.com>
2020-11-29 12:11:30 -0300

    simulador funcionando
    falta liberar as paginas em quadro no final

--- sim-virtual.c
+++ sim-virtual.c
@@ -1,225 +1,195 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
-#include <time.h> 
-
-#define N 20
-
 // Alunas:
 //  Mariana Medeiros Ruddy Santos   - 1611397
-//  Camilla Santos Celles           - XXXXXXX
+//  Camila Santos Celes             - 1810336
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 
 struct pagina {
-	char endereco;
-    char flag; // M (pagina modificada) ou R (pagina refernciada)
-    int ultimo_acesso;
-	struct pagina *prox;
-};
+  int endereco;
+  char R;
+  char M;
+  int ultimo_acesso;
+}; typedef struct pagina Pagina;
 
-char *tipo_algo;
+Pagina* emQuadro[500]; // armazena vetor de paginas que estao ja mapeadas 
 
-int page_faults = 0;
-int paginas_usadas = 0;
+char* algoritmo;
+FILE *arquivo_log;
+int s;
 int num_paginas;
-char *modo_debug = " ";
+int num_paginas_usadas = 0;
+int page_faults = 0;
+int paginas_sujas = 0;
+int conta_tempo = 0;
 
-struct pagina *primeira, *ultima;
-
-
-void adiciona_pagina(unsigned end, int i)
-{
-    if(strcmp(modo_debug, "debug")== 0 || strcmp(modo_debug, "-D")== 0) printf("\n-----adiciona pagina----\n");
-	struct pagina *atual = (struct pagina*)malloc(sizeof(struct pagina));
-    atual->endereco = end;
-    atual->prox = NULL;
-    atual->ultimo_acesso = i;
-
-    if(paginas_usadas == 0)
-    {
-        primeira = atual;
-        ultima = atual;
-    }
-    else
-    {
-        atual->prox = atual;
-        atual = atual;
-    }
-
-    if(paginas_usadas < num_paginas)
-    {
-        paginas_usadas++;
-    }
+int procuraPagina(int end) {
+  for (int i=0; i<num_paginas_usadas; i++) {
+    if (emQuadro[i]->endereco == end)
+      return i;
+  }
+  return -1;
 }
 
-void NOVO(unsigned end)
-{
-    //TODO
+void adicionaPagina(int end, char rw) {
+  page_faults++;
+
+  Pagina *p = (Pagina *)malloc(sizeof(Pagina));
+  p->endereco = end;
+  p->R = (rw == 'R');
+  p->M = (rw == 'M');
+  p->ultimo_acesso = conta_tempo;
+
+  emQuadro[num_paginas_usadas++] = p;
 }
 
-void NRU(unsigned end)
-{
-    //TODO
+int descartaLRU() {
+  int ind = 0;
+  for (int i=1; i<num_paginas; i++) { // procurando indice da menos recente
+    if (emQuadro[i]->ultimo_acesso < emQuadro[ind]->ultimo_acesso) {
+      ind = i;
+    }
+  }
+  return ind;
 }
 
-void LRU(unsigned end, int i)
-{
-    adiciona_pagina(end, i);
-    if(paginas_usadas == num_paginas)
-    {
-        primeira = primeira->prox;
-        primeira->ultimo_acesso = i;
+int descartaNRU() {
+  // procura por M=0, R=0
+  for (int i=1; i<num_paginas; i++) {
+    if (!emQuadro[i]->M && !emQuadro[i]->R) {
+      return i;
     }
+  }
+  // procura por M=1, R=0
+  for (int i=1; i<num_paginas; i++) {
+    if (emQuadro[i]->M && !emQuadro[i]->R) {
+      return i;
+    }
+  }
+  // procura por M=0, R=1
+  for (int i=1; i<num_paginas; i++) {
+    if (!emQuadro[i]->M && emQuadro[i]->R) {
+      return i;
+    }
+  }
+  return 0; // pega a primeira M=1, R=1
 }
 
-void substitui(unsigned end, int i)
-{
-    if(strcmp(modo_debug, "debug")== 0 || strcmp(modo_debug, "-D")== 0) printf("\n-----substitui----\n");
-    if(strcmp(tipo_algo, "LRU") == 0)
-    {
-        LRU(end, i);
-    }
-    else if(strcmp(tipo_algo, "NRU") == 0)
-    {
-        NRU(end);
-    }
-    else if(strcmp(tipo_algo, "NOVO") == 0)
-    {
-        // novo  algoritmo  de substituição  de  páginas ótimo imaginado  por  você
-        NOVO(end);
-    }
-    else
-    {
-        printf("algoritmo de Substituição de Páginas %s é inválido\n", tipo_algo);
-        exit(1);
-    }
-    page_faults++;
-}
+int descartaNOVO() {
+  // iremos "olhar para o futuro" e ver qual das paginas em quadro esta mais 
+  // longe de ser usada de novo e substituir essa (entre as 10 proximas)
 
-void escreve(unsigned end, int i)
-{
-    if(strcmp(modo_debug, "debug")== 0 || strcmp(modo_debug, "-D")== 0) printf("\n-----escreve----\n");
-    if(paginas_usadas < num_paginas)
-    {
-        adiciona_pagina(end, i);
-    }
-    else
-    {
-        substitui(end, i);
-    }
-}
+  // armazenando posicao do cursor na leitura do arquivo pela main para depois retornar a esse ponto
+  fpos_t cursor;
+  fgetpos(arquivo_log, &cursor);
 
-int procura(unsigned end){
-	struct pagina *temp = primeira, *ant = NULL;
-	while(temp != NULL){
-		if(temp->endereco == end){
-			if(strcmp(tipo_algo, "LRU") == 0){
-				if(ant != NULL){
-					if(temp->prox != NULL)
-						ant->prox = temp->prox;							
-				}
-				else {
-					primeira = primeira->prox;
-				}
-				ultima->prox = temp;
-				ultima = temp;
-				temp->prox = NULL;
-			}
-			return 0;
-		}
-		ant = temp;	
-		temp = temp->prox;
-	}
-	return 1;
-}
-
-
-int main(int argc, char *argv[])
-{
-    // parametros de chamada do programa
-    tipo_algo = argv[1];
-    char *nome_arquivo = argv[2];
-    int tam_pagina = atoi(argv[3]);
-    int tam_mem_fisica = atoi(argv[4]);
-    if(argv[5])
-    {
-        modo_debug = argv[5];
-    }
-    
-    num_paginas = tam_mem_fisica / tam_pagina; 
-
-    int i = 0;
-    int s = 13;
-
-    FILE *pnt_arquivo;
-    char rw;
-    unsigned end;
-    int end_pagina;
-    int pags_escritas = 0;
-    int pags_faltantes = 0;
-
-    if(tam_pagina == 8)
-    {
-        s += 0;
-    }
-    else if(tam_pagina == 16)
-    {
-        s += 1;
-    }
-    else if(tam_pagina == 32)
-    {
-        s += 2;
-    }
-    else
-    {
-        printf("Tamanho de Página %d é inválido\n", tam_pagina);
-        exit(1);
-    }
-
-    pnt_arquivo = fopen(nome_arquivo, "r");
-    if(pnt_arquivo == NULL)
-    {
-        printf("Erro ao abrir arquivo %s\n", nome_arquivo);
-        exit(1);
-    }
-
-    if(strcmp(modo_debug, "debug")== 0 || strcmp(modo_debug, "-D")== 0) printf("\n-----MODO DEBIG ATIVADO----\n");
-    printf("\nExecutando o simulador...\n");
-    printf("Arquivo de entrada: %s\n", nome_arquivo);
-    printf("Tamanho da memoria fisica: %d\n", tam_mem_fisica);
-    printf("Tamanho das páginas: %d\n", tam_pagina);
-    printf("Alg de substituição: %s\n", tipo_algo);
-
-    while(fscanf(pnt_arquivo, "%x %c\n", &end, &rw) != -1)
-    {
-        // printf("%d: %x %c\n", i, end, rw);
-        end_pagina = end >> s;
-        if(strcmp(&rw, "W") == 0)
-        {
-            escreve(end, i);
+  char flags[500]; for(int i=0; i<num_paginas; i++) flags[i] = 0;
+  unsigned addrs; char rw;
+  int count = 0;
+  int count_linhas = 0;
+  while (count_linhas < 10 && fscanf(arquivo_log, "%x %c\n", &addrs, &rw) != -1) {
+    int end_pagina = addrs >> s;
+    for (int i=0; i<num_paginas && count<num_paginas-1; i++) {
+      if (emQuadro[i]->endereco == end_pagina) { // uma das paginas em quadro foi encontrada "no futuro"
+        if (!flags[i]) {
+          flags[i] = 1;
+          count++;
         }
-
-        if(strcmp(&rw, "R") == 0)
-        {
-            if(procura(end) != 0)
-            {
-                escreve(end, i);
-            }
-        }
-        i++;
+      }
     }
+    count_linhas++;
+  }
+  int ind;
+  for (int i=0; i<num_paginas; i++) {
+    if (!flags[i]) {
+      ind = i; // vamos retornar a pagina que nao foi encontrada no futuro proximo (flag = 0)
+      break;
+    }
+  }
 
-    // finalizacao do programa
-    printf("Numero de Faltas de Páginas: %d\n", pags_faltantes);
-    printf("Numero de Paginas escritas: %d\n", pags_escritas);
-    fclose(pnt_arquivo);
-	struct pagina *tmp = primeira;
-	while(tmp != NULL){
-		free(tmp);
-		tmp = tmp->prox;
-	}
-    printf("a execução demorou %d de unidades de tempo\n", i);
-    return 0;
-};
+  fsetpos(arquivo_log, &cursor); // voltando com o cursor da main para onde estava
+  return ind;
+}
 
-// gcc sim-virtual.c -o sim-virtual && ./sim-virtual LRU Todos-Arquivos/compilador.log 8 16
+void descartaPagina() {
+  int ind;
+  // procurando qual descartar segundo algoritmo de substituicao
+  if (strcmp(algoritmo, "LRU") == 0) {
+    ind = descartaLRU();
+  }
+  else if (strcmp(algoritmo, "NRU") == 0) {
+    ind = descartaNRU();
+  }
+  else { // algoritmo == NOVO
+    ind = descartaNOVO();
+  }
+
+  // descartando a pagina do vetor de em quadro, substituindo pela ultima posicao do vetor
+  if (emQuadro[ind]->M)
+    paginas_sujas++;
+  free(emQuadro[ind]);
+  emQuadro[ind] = emQuadro[--num_paginas_usadas];
+}
+
+void paginaLida(int end, char rw) {
+  int indPagina = procuraPagina(end);
+  if (indPagina >= 0) { // pagina ja em quadro, atualiza seus dados
+    if (rw == 'R') {
+      emQuadro[indPagina]->R = 1;
+   } else {
+      emQuadro[indPagina]->M = 1;
+    }
+    emQuadro[indPagina]->ultimo_acesso = conta_tempo;
+  } 
+  else { // pagina nao em quadro, a adiciona
+    if (num_paginas_usadas == num_paginas)
+      descartaPagina();
+    adicionaPagina(end, rw);
+  }
+}
+
+
+int main(int argc, char *argv[]) {
+  // lendo parametros da chamada do simulador
+  algoritmo = argv[1];
+  char *filename = argv[2];
+  int tam_pagina = atoi(argv[3]);
+  int tam_mem_fisica = atoi(argv[4]);
+
+  num_paginas = tam_mem_fisica / tam_pagina;
+
+  s = 13;
+  if(tam_pagina == 8) 
+    s += 0;
+  else if(tam_pagina == 16)
+    s += 1;
+  else if(tam_pagina == 32)
+    s += 2;
+  else {
+    printf("Tamanho de Página %d é inválido\n", tam_pagina);
+    exit(1);
+  }
+
+  arquivo_log = fopen(filename, "r");
+  if(arquivo_log == NULL) {
+    printf("Erro ao abrir arquivo %s\n", filename);
+    exit(1);
+  }
+
+  printf("\nExecutando o simulador...\n");
+  printf("Arquivo de entrada: %s\n", filename);
+  printf("Tamanho da memoria fisica: %d\n", tam_mem_fisica);
+  printf("Tamanho das páginas: %d\n", tam_pagina);
+  printf("Alg de substituição: %s\n", algoritmo);
+
+  unsigned addrs; char rw;
+  while (fscanf(arquivo_log, "%x %c\n", &addrs, &rw) != -1) {
+    int end_pagina = addrs >> s;
+    paginaLida(end_pagina, rw);
+    conta_tempo++;
+  }
+
+  printf("Numero de Faltas de Páginas: %d\n", page_faults);
+  printf("Numero de Paginas escritas: %d\n", paginas_sujas);
+}
